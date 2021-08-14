5f0b4d4efd45b536ae0f60c6ff3bc6812bbf4c0e
GuJa Kheladze <kkkheladze@GuJas-MacBook-Air.local>
2021-08-14 11:25:34 +0400

    initial score component setup

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./score.component.sass']
 })
 export class ScoreComponent implements OnInit {
-
+  score!:number;
   constructor() { }
 
   ngOnInit(): void {
+    this.score=12
   }
 
 }
