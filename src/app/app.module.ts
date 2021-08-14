d7e17f9a3f6c9d24bb9b3de030496859a86eb058
GuJa Kheladze <kkkheladze@GuJas-MacBook-Air.local>
2021-08-14 10:00:33 +0400

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScoreComponent } from './score/score.component';
+import { MovesComponent } from './moves/moves.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoreComponent,
+    MovesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/moves/moves.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moves',
+  templateUrl: './moves.component.html',
+  styleUrls: ['./moves.component.sass']
+})
+export class MovesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
